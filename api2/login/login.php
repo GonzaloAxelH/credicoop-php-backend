40fcc1329c5bfc1e37cacf55c266cfac249235b6
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-25 17:52:38 -0500

    arreglando errores del login

--- api2/login/login.php
+++ api2/login/login.php
@@ -1,11 +1,26 @@
 <?php 
 
 include('../conexion/jwt.php');
+include ('../conexion/Conexion.php');
 header('Access-Control-Allow-Origin: *');
+
 $data = json_decode(file_get_contents('php://input'),true);
+echo "no encontro";
 if(isset($data["codigo"]) && $data["password"]){
 	$codigo =$data["codigo"];
 	$password = $data["password"];
-	$resultado = createToken($codigo,$password);
-	echo $resultado;
+
+	echo "hola";
+
+	$resultado = mysqli_query($conexion,$consulta);
+	$consulta = "SELECT id_ccliente FROM cuentacliente WHERE contrasena='$password' AND idcliente='$codigo'";
+	
+	$row_cnt = mysqli_num_rows($resultado);
+	if($row_cnt > 0){
+		$resultado = createToken($codigo,$password);
+		echo $resultado;
+	}else{
+		echo json_encode(array("message" => 'Usuario o contraseña incorrectos'));
+	}
+	
 }
