e9551bdbdfc2bc267b5e35e3884db1bb2cb2b218
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-25 18:38:47 -0500

    make it better

--- api2/login/login.php
+++ api2/login/login.php
@@ -2,20 +2,15 @@
 
 include('../conexion/jwt.php');
 include ('../conexion/Conexion.php');
-
 header('Access-Control-Allow-Origin: *');
-
-
-
-echo "sdsd";
-
-
+echo "";
 $data = json_decode(file_get_contents('php://input'),true);
 if(isset($data["codigo"]) && $data["password"]){
+	
 	$codigo =$data["codigo"];
 	$password = $data["password"];
+	$consulta ="SELECT id_ccliente FROM cuentacliente WHERE idcliente='$codigo' AND contrasena='${password}'";
 	$resultado = mysqli_query($conexion,$consulta);
-	$consulta = "SELECT id_ccliente FROM cuentacliente WHERE contrasena='$password' AND idcliente='$codigo'";
 	$row_cnt = mysqli_num_rows($resultado);
 	if($row_cnt > 0){
 		$resultado = createToken($codigo,$password);
