bbeaa99b731d276ba4e396133f64b2e8f0eda620
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-25 18:13:11 -0500

    make it better

--- api2/login/log.php
+++ api2/login/log.php
@@ -3,5 +3,20 @@
 	include ('../conexion/Conexion.php');
 
 	header('Access-Control-Allow-Origin: *');
-	echo "gonzalo___";
+
+$data = json_decode(file_get_contents('php://input'),true);
+if(isset($data["codigo"]) && $data["password"]){
+	$codigo =$data["codigo"];
+	$password = $data["password"];
+	$resultado = mysqli_query($conexion,$consulta);
+	$consulta = "SELECT id_ccliente FROM cuentacliente WHERE contrasena='$password' AND idcliente='$codigo'";
+	$row_cnt = mysqli_num_rows($resultado);
+	if($row_cnt > 0){
+		$resultado = createToken($codigo,$password);
+		echo $resultado;
+	}else{
+		echo json_encode(array("message" => 'Usuario o contraseña incorrectos'));
+	}
+}
+
 ?>
