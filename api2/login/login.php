f4710f2ad1a7ed3dd070ab0383c892492d880f4b
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-27 23:20:45 -0500

    make it better

--- api2/login/login.php
+++ api2/login/login.php
@@ -15,9 +15,7 @@ if(isset($data["codigo"]) && $data["password"]){
 	$row_cnt = mysqli_num_rows($resultado);
 	if($row_cnt > 0){
 		$jwt= createToken($codigo,$password);
-		echo $row_cnt;
-
-		echo json_encode(array("jwt" =>$jwt,"codigo"=> $codigo));
+		echo json_encode(array('jwt' => $jwt,'codigo'=> $codigo));
 	}else{
 		echo json_encode(array("message" => 'Usuario o contraseña incorrectos'));
 	}
