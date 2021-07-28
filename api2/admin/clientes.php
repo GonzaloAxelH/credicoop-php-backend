371af8b37f4de30ba31204b93870cc17731182f5
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-27 19:03:28 -0500

    make it better

--- api2/admin/clientes.php
+++ api2/admin/clientes.php
@@ -9,8 +9,8 @@ $data = json_decode(file_get_contents('php://input'),true);
 if($data['ID']){
 	echo json_encode($data);
 }else{
-	//$resultado = getTable("cliente",$conexion);
-	//echo json_encode($resultado);
+	$resultado = getTable("cliente",$conexion);
+	echo json_encode($resultado);
 }
 
 
