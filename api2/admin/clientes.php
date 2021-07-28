bca82f2a4342b54f5e642938c0eb7c734622c087
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-27 19:02:42 -0500

    make it better

--- api2/admin/clientes.php
+++ api2/admin/clientes.php
@@ -6,7 +6,12 @@ header('Access-Control-Allow-Origin: *');
 $data = json_decode(file_get_contents('php://input'),true);
 
 
+if($data['ID']){
+	echo json_encode($data);
+}else{
+	//$resultado = getTable("cliente",$conexion);
+	//echo json_encode($resultado);
+}
 
-echo json_encode($data);
-//$resultado = getTable("cliente",$conexion);
-//echo json_encode($resultado);
+
+
