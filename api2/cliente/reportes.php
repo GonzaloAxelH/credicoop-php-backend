a80d7917667b315a2ae435aa41e776b40a00b963
GonzaloAxelH <gonzaloaxelh@gmail.com>
2021-07-28 20:22:01 -0500

    dssd

--- api2/cliente/reportes.php
+++ api2/cliente/reportes.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 $data = json_decode(file_get_contents('php://input'),true);
 	if(isset($data["idUser"])){
 		$idUser = $data["idUser"];
-        $sql="SELECT id_trans,monto,fecha_trans FROM transaccion WHERE idcliente='$idUser'";
+        $sql="SELECT * FROM transaccion WHERE idcliente='$idUser'";
         $query = $conexion->query($sql);
         $resultado = $query->fetch_all(MYSQLI_ASSOC);
 	$json = array();
